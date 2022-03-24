c33b8cce22beb496ff27ad7bc63afa84012e4165
babinicz <babinicz@onet.pl>
2022-03-24 19:54:26 +0100

    modal

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,11 +5,16 @@ import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Modal from '../Portal/Modal'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = ['W', 'o', 'ł', 'o', 'd', 'y', 'm', 'y', 'r']
   const jobArray = ['Z', 'e', 'ł', 'e ', 'ń', 's', 'k', 'i']
+
+  const [modal, setModal] = useState(false)
+  const Toggle = () => setModal(!modal)
+
   useEffect(() => {
     return setTimeout(() => {
       setLetterClass('text-animate-hover')
@@ -57,9 +62,32 @@ const Home = () => {
             />
           </h2>
           <h3>Wygramy, powstaną nowe domy, miasta, marzenia ...</h3>
-          <Link to="/contact" className="flat-button">
+          <Link to="" className="flat-button" onClick={() => Toggle()}>
             Więcej
           </Link>
+          <Modal
+            show={modal}
+            close={Toggle}
+            title="Zełenski: Nie mówcie nam, że nasza armia nie spełnia standardów NATO"
+          >
+            <p>
+              - Miesiąc heroicznego oporu. Miesiąc najciemniejszego cierpienia.
+              Miesiąc bezkarnego niszczenia pokojowego kraju, a razem z nim -
+              całej architektury globalnego bezpieczeństwa. To wszystko jest
+              przed światem - powiedział prezydent. Wołodymyr Zełenski
+              powtórzył, że w swoim oporze wobec Rosji Ukraina potrzebuje
+              nieograniczonej pomocy wojskowej, w sytuacji, gdy Moskwa
+              wykorzystuje cały posiadanego przez nią arsenał, którym atakuje
+              wszystko - od domów po kościoły, od magazynów żywności po
+              uniwersytety, od mostów po szpitale.
+            </p>
+            <br />
+            <p>
+              <em>
+                www.rp.pl/konflikty-zbrojne/art35933521-zelenski-nie-mowcie-nam-ze-nasza-armia-nie-spelnia-standardow-nato
+              </em>
+            </p>
+          </Modal>
         </div>
         <Logo />
       </div>
