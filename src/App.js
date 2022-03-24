41d5c583b6efb40b8834eed90ec7dfe247678d41
babinicz <babinicz@onet.pl>
2022-03-24 19:57:06 +0100

    Scroll

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
